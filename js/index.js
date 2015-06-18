f6ab6af124b49bcd0d8135b2d0f3d40360d4507e
Juliet Brown <julietbrown84@gmail.com>
2015-06-19 10:59:14 +1200

    fixedfont in header

--- js/index.js
+++ js/index.js
@@ -32,36 +32,8 @@ $(document).ready(function(){
 
   init();
 
-    var current_h = null;
-   var current_w = null;
-
-  $('.expand').hover(
-      function(){
-          current_h = $(this, 'img')[0].height;
-          current_w = $(this, 'img')[0].width;
-          $(this).stop(true, false).animate({width: (current_w * 1), height: (current_h * 1.3)}, 300);
-      },
-      function(){
-          $(this).stop(true, false).animate({width: current_w + 'px', height: current_h + 'px'}, 300);
-      }
-  );
-
-
+   
 })();
 
 
    
-//   var current_h = null;
-//   var current_w = null;
-
-//   $('.expand').hover(
-//       function(){
-//           current_h = $(this, 'img')[0].height;
-//           current_w = $(this, 'img')[0].width;
-//           $(this).stop(true, false).animate({width: (current_w * 1), height: (current_h * 1.3)}, 300);
-//       },
-//       function(){
-//           $(this).stop(true, false).animate({width: current_w + 'px', height: current_h + 'px'}, 300);
-//       }
-//   );
-// });
