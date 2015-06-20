7d95104abd146ba4a12f54b634a8a0cc97e9e0af
Juliet Brown <julietbrown84@gmail.com>
2015-06-21 11:31:06 +1200

    changed files

---
+++ js/process.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+  $('button').click(function() {
+      var toAdd = $("input[name=message]").val();
+      $('#messages').append("<p>" + toAdd+ "</p>");
+    });
+  });
+
+  $('nav').hover(
+        function(){
+            $(this).addClass('active');
+        },
+        function(){
+            $(this).removeClass('active')
+        }
+     );
+});
